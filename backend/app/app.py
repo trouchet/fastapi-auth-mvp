e1628d62d2771b9a4b6d1dbac09ee2095324c7ab
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-22 10:56:32 -0300

    feat: new routes

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
-from backend.app.routes import router
+from backend.app.routes import (
+    auth_router, data_router, misc_router,
+)
 
 app = FastAPI()
 
 # Include the router in the app
-app.include_router(router)
+app.include_router(auth_router)
+app.include_router(misc_router)
+app.include_router(data_router)
