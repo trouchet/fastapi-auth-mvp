bd484e5214245176c32eb61d01f27812fb475ca9
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-24 15:36:41 -0300

    refactor: increase timeout for access token

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -2,10 +2,10 @@
 from datetime import timedelta
 
 # Constants for the application.
-POSSIBLE_ROLES_SET = {"admin", "user"}
+POSSIBLE_ROLES_SET={'user', 'admin'}
 
 # Token expiration times
-DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES = 20
+DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES = 60
 DEFAULT_REFRESH_TOKEN_EXPIRE_MINUTES = 120
 
 DEFAULT_ACCESS_TIMEOUT_MINUTES = timedelta(minutes=DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES)
