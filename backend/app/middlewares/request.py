e33229521b67169eacc5148664ba5e3d9a27fdc8
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-30 18:15:22 -0300

    feat: log only necessary requests

--- backend/app/middlewares/request.py
+++ backend/app/middlewares/request.py
@@ -1,23 +1,45 @@
-from fastapi import Request, Depends
+from fastapi import Request
+from fastapi.responses import Response
 from starlette.middleware.base import BaseHTTPMiddleware
 from typing import Callable
 
-from backend.app.repositories.request import RequestLogRepository
+
+from backend.app.repositories.request import (
+    RequestLogRepository,
+)
 from backend.app.database.instance import get_session
 
+def should_log_request(request: Request, response: Response):
+    # Log only POST, PUT, DELETE requests or error responses
+    log_verbs = ['POST', 'PUT', 'DELETE']
+    is_log_verb = request.method in log_verbs
+    is_error_response = response.status_code >= 400
+
+    return is_log_verb or is_error_response
+        
+
 class RequestLoggingMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next: Callable):
+    async def dispatch(
+        self, request: Request, call_next: Callable
+    ):
         session=get_session()
         request_repo=RequestLogRepository(session)
-        try:
-            # Log request information
-            await request_repo.create_log(request)
 
-            # Process the request
-            response = await call_next(request)
+        # Process the request
+        response = await call_next(request)
+
+        try:
+            if should_log_request(request, response):
+                # Log request information
+                await request_repo.create_log(request)
+
             return response
         except Exception as e:
             session.rollback()
             raise e
         finally:
             session.close()
+
+
+
+
