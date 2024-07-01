9ba3221c297674c5b65851d9a5759c6d189232d9
Bruno Peixoto <brunolnetto@gmail.com>
2024-07-01 16:35:27 -0300

    feat: scheduler for log cleanup

--- backend/app/scheduler/request_logging.py
+++ backend/app/scheduler/request_logging.py
@@ -6,18 +6,16 @@ from backend.app.repositories.request import (
 )
 from backend.app.database.instance import get_session
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 scheduler = AsyncIOScheduler()
 
 async def delete_old_logs(retention_period_days=30):
-    session = get_session()
-    
     cutoff_date = datetime.now() - timedelta(days=retention_period_days)
 
-    datetime_filter = RequestLog.relo_timestamp < cutoff_date
-    session.query(RequestLog).filter(datetime_filter).delete()
-    session.commit()
-    session.close()
+    with get_session() as session:
+        datetime_filter = RequestLog.relo_timestamp < cutoff_date
+        session.query(RequestLog).filter(datetime_filter).delete()
+        session.commit()
+        session.close()
 
 
 async def setup_log_cleanup():
