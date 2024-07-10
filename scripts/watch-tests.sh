bb4ecb2ad2ee53c68b9fd38e53d61578c58229b5
Bruno Peixoto <brunolnetto@gmail.com>
2024-07-10 17:53:06 -0300

    config: add trafik and watch-tests script

--- scripts/watch-tests.sh
+++ scripts/watch-tests.sh
@@ -3,12 +3,14 @@ clear
 
 # Define the omitted paths
 OMIT_PATHS="backend/tests/*,*/__init__.py"
-SLEEP_TIME=10
+SLEEP_TIME_S=20
+LOG_FILE="test_log.txt"  # Output filename for the test log
+
 
 while true; do
     coverage run --rcfile=.coveragerc -m pytest
-    coverage report --omit="$OMIT_PATHS" --show-missing
+    coverage report --omit="$OMIT_PATHS" --show-missing &> "$LOG_FILE"
 
-    sleep $SLEEP_TIME
+    sleep $SLEEP_TIME_S
     clear
 done
