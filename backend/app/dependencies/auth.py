edc3419c9c6a3b78d33e6ed50826e945554c1a17
Bruno Peixoto <brunolnetto@gmail.com>
2024-07-10 20:17:59 -0300

    feat: create a dependencies folder

---
+++ backend/app/dependencies/auth.py
@@ -0,0 +1,13 @@
+from typing import Annotated, Tuple
+from fastapi import Depends
+from backend.app.core.auth import validate_refresh_token
+
+from backend.app.models.users import User
+from backend.app.repositories.auth import get_role_repository
+
+RefreshTokenDependency = Annotated[
+    Tuple[User, str], Depends(validate_refresh_token)
+]
+
+RoleRepositoryDepends = Depends(get_role_repository)
+
